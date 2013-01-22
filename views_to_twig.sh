7bbe2e0f9f2408070b3b86070b3985fa17348094
Nico Rodsevich <neton_12@hotmail.com>
2013-01-22 20:15:34 -0300

    Elimination of obsolete tests files, changed name to english and improved the code to a usable one

--- views_to_twig.sh
+++ views_to_twig.sh
@@ -6,7 +6,7 @@ E_NO_FILE_CREATION=203;
 
 m_E_WRONGARGS="El primer parametro, en donde se especifica el directorio a reemplazar los templates, es obligatorio";
 m_E_NO_CD="No pudo posicionarse en $DIR.";
-m_E_NO_FILE_CREATION="No se puedo crear el archivo";
+m_E_NO_FILE_CREATION="No se pudo crear el archivo";
 m_modifying="Modificando";
 m_modify_success="\e[01;32mMODIFICADO\e[00m";
 m_modify_none="\e[01;33mSIN CAMBIOS\e[00m";
@@ -33,12 +33,17 @@ fi
 
 #sed args
 sed_args=()
+#Tranforms echos structures
+sed_args+=("-e 's/<?\(php\)\? \?\(=\|echo\) \?/{{ /g' -e 's/\({{.*\);\? \??>/\1}}/g'")
 
-sed_args+=("-e 's/<?\(php\)\? \?\(=\|echo\) \?/\{\{ /g' -e 's/\({{.*\)?>/\1 }}/g'")
+#Transforms control structures
+sed_args+=("-e 's/<?\(php\)\? \?/{% /g' -e 's/\({%.*\)[;:{] \??>/\1%}/g'")
 
-sed_args+=("-e 's/<?\(php\)\? \?/{% /g' -e 's/\({%.*\)[;:{] ?>/\1 %}/g'")
+#Transforms variables in structures
+sed_args+=("-e 's/\(\({{\|{%\).*\)\\$\(.*\)->\(is\|get\)\(.*\)$/\1\3.\5/g' -e 's/\({{\|{%\)\(.*\)\\$/\1\2/g'")
 
-sed_args+=("-e 's/\({{.*\)\\$\([:alpha:]*\)/\1 REEMPLAZO /g' -e 's/&/AMPERSAND/g'")
+#Fixes identation
+sed_args+=("-e 's/ *\(}}\|%}\)/ \1/g'")
 
 for arch in *.php
 do
@@ -48,7 +53,9 @@ do
 		echo -n $m_modifying; echo -en "\e[01;38m `basename $arch`... \e[00m"
 		
 		twig="$arch.twig"
-		echo "sed "${sed_args[@]}" $arch > $twig"
+		echo "sed ${sed_args[@]} $arch > $twig"
+		eval "sed ${sed_args[@]} $arch > $twig"
+		#sed "${sed_args[@]}" $arch > $twig
 		
 		if [ -f "$twig" ]
 		then
