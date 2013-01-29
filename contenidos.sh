87c3a85a67f2cc58c29dec82b1d0c4425a7e2365
Rodsevich <neton_12@hotmail.com>
2013-01-28 21:12:22 -0300

    cambios al pedo en contenidos.sh

--- contenidos.sh
+++ contenidos.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 cd /var/www/Proyecto/frontend/controller
+
 for arch in *.php
 do
-	output+="$( cat $arch | grep /views/)"
+	echo -e "\n ------------   $arch   ---------------\n"
+	matches=$(cat $arch | grep /views/)
+	echo -e "$matches"
+	vars=$(echo -e "$matches" | grep -e "^[[:blank:]]*\\$.*=")
+	echo -e "Vars: $vars"
 done
-echo -e "$output"
-vars=$(echo -e "$output" | grep -e "\\$.*=")
-	echo -e "$vars"
+
